f1461e7aa504d7a5dc2530b315b5a6f68f44cdab
nadhemDev <blendplay2@gmail.com>
2022-12-20 15:05:12 +0100

    Session config

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -4,15 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
     #[Route('/todo', name: 'app_todo')]
-    public function index(): Response
+    public function index(Request $request): Response
     {   
 
-        //Afficher notre table de  todo
+        $session = $request->getSession();
+        if(!$session->has('todos'))
+        {
+            $todos = [
+                'achat' =>'acheter clé usb',
+                'cours'=>'Finaliser mon cours',
+                'correction'=>'corriger mes examens'
+            ];
+            $session->set('todos' , $todos);
+        }
         return $this->render('todo/index.html.twig', [
             'controller_name' => 'TodoController',
         ]);
